8b4690e8bae4876e2c2f09edd9e0b29e3c2fc356
Shane <Shane@pamelasrtzsimac.home>
2019-02-18 13:32:34 -0500

    finished act2 code, needto do qs

--- Activity2 Starter Code/Deck.java
+++ Activity2 Starter Code/Deck.java
@@ -30,12 +30,14 @@ public class Deck {
 	 * @param values is an array containing all of the card point values.
 	 */
 	public Deck(String[] ranks, String[] suits, int[] values) {
-		//cards = new ArrayList<>;
-		for(int i = 0; i < 3; i++) {
-			Card me = new Card(ranks[i], suits[i], values[i]);
-			cards.set(i, me);
+		cards = new ArrayList<Card>();
+		for(int i = 0; i < ranks.length; i++) {
+			for(String suit : suits) {
+				cards.add(new Card(ranks[i], suit, values[i]));
+			}
+			
 		}
-		
+		size = cards.size();
 		shuffle();
 	}
 
@@ -45,10 +47,10 @@ public class Deck {
 	 * @return true if this deck is empty, false otherwise.
 	 */
 	public boolean isEmpty() {
-		if(this.size() == 0) {
-			return false;
+		if(size == 0) {
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 	/**
@@ -73,6 +75,9 @@ public class Deck {
 	 *         previously dealt.
 	 */
 	public Card deal() {
+		if(isEmpty()) {
+			return null;
+		}
 		size--;
 		return cards.get(size);
 		
--- Activity2 Starter Code/DeckTester.java
+++ Activity2 Starter Code/DeckTester.java
@@ -9,9 +9,11 @@ public class DeckTester {
 	 */
 	public static void main(String[] args) {
 		String[] ranks = {"Two", "Seven", "Three"};
-		String[] suits = {"Spade", "Club", "Diamond"};
+		String[] suits = {"Black", "Red"};
 		int[] values = {2, 7, 3};
 		Deck cards = new Deck(ranks, suits, values);
+		System.out.println(cards.deal());
+		System.out.println(cards.deal());
 		System.out.println(cards.deal());
 	}
 }
